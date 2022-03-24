eb70f4b29325fe69d2e1b070770dfa5b65689cad
symbiologist <symbiologist@gmail.com>
2022-03-23 19:02:52 -0700

    Update scripts

--- process.R
+++
@@ -1,5 +0,0 @@
-file.copy(from = '/media/data4/shared/ipsc_manuscript/analysis/reference/output/002_unified_reference/unified_ucsc.gtf',
-          to = 'data', overwrite = TRUE)
-
-# Add this text
-# track name="Unified Reference" description="Unified Reference lncRNA genes" color=204,0,204
---
+++ scripts/expression.R
@@ -0,0 +1,16 @@
+setwd('/media/data1/dwu/hosting/')
+
+source <- '/media/data4/shared/ipsc_manuscript/analysis/perturbseq/output/webapp_plots/expression/'
+
+files_to_copy <- list.files(source)[1:8000]
+
+file.copy(from = file.path(source, files_to_copy),
+          to = 'data/expression/', 
+          overwrite = TRUE, recursive = TRUE)
+
+
+length(list.files(source))
+
+#github
+usethis::use_git_config(user.name = "symbiologist", user.email = "symbiologist@gmail.com")
+credentials::set_github_pat("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")
